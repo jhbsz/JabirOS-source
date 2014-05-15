4fad9b6310a0f2ff0bcc5a69717934c5a800a9da
Muhammadreza <haghiri75@gmail.com>
2014-05-15 21:15:24 +0430

    Code Base

---
+++ src/crypto/openssh/version.h
@@ -0,0 +1,10 @@
+/* $OpenBSD: version.h,v 1.68 2013/11/08 01:38:11 djm Exp $ */
+/* $FreeBSD: release/10.0.0/crypto/openssh/version.h 258343 2013-11-19 11:47:30Z des $ */
+
+#define SSH_VERSION	"OpenSSH_6.4"
+
+#define SSH_PORTABLE	"p1"
+#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
+
+#define SSH_VERSION_FREEBSD	"FreeBSD-20131111"
+#define SSH_VERSION_HPN		"_hpn13v11"
---
+++ src/sys/i386/include/ptrace.h
@@ -0,0 +1,6 @@
+/*-
+ * This file is in the public domain.
+ */
+/* $FreeBSD: release/10.0.0/sys/i386/include/ptrace.h 232520 2012-03-04 20:24:28Z tijl $ */
+
+#include <x86/ptrace.h>
